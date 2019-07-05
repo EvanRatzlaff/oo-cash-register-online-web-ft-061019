155a63d6dac861769734de410eef2336c6fdef49
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-07-05 17:12:10 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,11 +8,8 @@ class CashRegister
     self.last.transaction = {}
   end 
   def add_item(item, price, quantity = 1)
-    quantity.times do 
-      self.items << item
-    end
-    self.last_transaction = {price: price, quantity: quantity}
-    self.total = self.total.to_f + price * quantity
+    self.items << item 
+    self.total = self.total.to_f +price * quantity
   end 
   def apply_discount
     return "There is no discount to apply." if self.dicount == 0
@@ -20,6 +17,9 @@ class CashRegister
     "After the discount, the total comes to $#{self.total.to_i}"
   end 
   def void_last_transaction
-    self.total = self.total - self.last_transaction
+    self.total = self.total - self.last_transaction[:price]
+    self.last_transaction[:quantity].times do 
+      self.items.pop
+    end 
   end 
 end 
